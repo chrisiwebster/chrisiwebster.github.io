49fa9b2374571fda690c19406137a85a4bf4b934
Chrisi Webster <christinawebster.cw@gmail.com>
2020-10-22 18:14:03 +0100

    Updates

---
+++ precache-manifest.acf00734d3ea458ac02d753c5ed96727.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9875b6e8cd49ef935f5d222eda670ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2813fb3a8ad9782fd043",
+    "url": "/static/css/main.c6c60e5d.chunk.css"
+  },
+  {
+    "revision": "914f08ea86117c707d34",
+    "url": "/static/js/2.0b2b3fde.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.0b2b3fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2813fb3a8ad9782fd043",
+    "url": "/static/js/main.d5a852b3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "0fc9d143732cd11d6fbeb4e2fcbc07f0",
+    "url": "/static/media/CSS3.0fc9d143.png"
+  },
+  {
+    "revision": "ef019ac071ef2117081960733edd53fb",
+    "url": "/static/media/favicon.ef019ac0.ico"
+  },
+  {
+    "revision": "6ed98bb887b0c6bd715c564510a1b0c7",
+    "url": "/static/media/javascript.6ed98bb8.png"
+  },
+  {
+    "revision": "5183c484139c3d74c3c2d18929b4b4fb",
+    "url": "/static/media/me.5183c484.jpeg"
+  }
+]);
